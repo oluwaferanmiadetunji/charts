41aacc7875a4e5185573417ceca9242c407c5334
oluwaferanmiadetunji <oluwaferanmiadetunji@gmail.com>
2020-12-06 16:13:27 +0100

    Fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,20 +47,21 @@ const data = {
     '',
     '',
     '',
+    '',
+    '',
+    '',
+    '',
+    '',
   ],
   datasets: [
     {
-      label: '',
       fill: true,
       lineTension: 0.5,
       backgroundColor: 'pink',
       borderColor: 'red',
-      borderDash: [],
       borderDashOffset: 0.0,
       pointBorderColor: 'red',
-      pointBorderWidth: 0,
       pointHoverBackgroundColor: 'red',
-      pointHoverBorderWidth: 2,
       pointRadius: 0,
       spanGaps: false,
       data: [
@@ -104,10 +105,12 @@ const data = {
         3.31,
         3.33,
         3.35,
+        3.34,
         3.33,
+        3.32,
+        3.33,
+        3.34,
         3.35,
-        3.37,
-        3.39,
         3.4,
       ],
     },
@@ -123,19 +126,44 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Chart</h1>
+        <h1 className={styles.title}>Liquidity</h1>
+        <h1 className={styles.caption}>$3.46B <span className={styles.text}>+3.64%</span></h1>
         <Line
           data={data}
           width={500}
           height={300}
           options={{
+            legend: {
+              display: false
+          },
             scales: {
               yAxes: [
                 {
                   display: true,
                   position: 'right',
                   ticks: {
-                    beginAtZero: false,
+                    min: 3.0,
+                    max: 3.4,
+                    stepSize: 0.1,
+                    suggestedMin: 3.0,
+                    suggestedMax: 3.4,
+                    callback: function(label, index, labels) {
+                      switch (label) {
+                        case 3.0:
+                          return '$3.0B';
+                        case 3.1:
+                          return '$3.1B';
+                        case 3.2:
+                          return '$3.2B';
+                        case 3.3:
+                          return '$3.3B';
+                        case 3.4:
+                          return '$3.4B';
+                      }
+                    }
+                  },
+                  gridLines: {
+                    display: false
                   },
                   stepSize: 0.2,
                   suggestedMax: 3.5,
